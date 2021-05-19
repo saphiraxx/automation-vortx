01fbce980ba548be07c779ac18d8c25bf1ca8455
saphiraxx <saphiracaxa991@gmail.com>
2021-05-19 19:01:17 -0400

    webpack error

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,3 +3,10 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 }
+
+const  allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = (on, config )  =>  { 
+    allureWriter(on, config ) ; 
+    return config ; 
+}
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+import '@shelex/cypress-allure-plugin';
+//you can use require:
+require ('@shelex/ ypress-allure-plugin');
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
